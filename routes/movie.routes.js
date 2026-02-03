8555d1dbb3ad24cd5b60b3de9c33acb847028e25
Mehak <mehak.apiit@gmail.com>
2026-02-03 20:06:35 +0530

    Feat: Added client side bad request checks for create movie using middlewares

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -1,6 +1,7 @@
 import MovieController from "../controllers/movie.controller.js";
+import MovieMiddlewares from "../middlewares/movie.middlewares.js"
 
 const routes =(app) =>{
-    app.post('/mba/api/vi/movies',MovieController);
+    app.post('/mba/api/vi/movies',MovieMiddlewares,MovieController);
 };
 export default routes
