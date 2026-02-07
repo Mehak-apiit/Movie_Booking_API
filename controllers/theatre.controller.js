2c8df5a2ca0ce8a3214585d9a5b47da1202a0ada
Mehak <mehak.apiit@gmail.com>
2026-02-07 21:52:26 +0530

    Feat: handled validation failure for properties in theatre schema

--- controllers/theatre.controller.js
+++ controllers/theatre.controller.js
@@ -3,6 +3,11 @@ import {successResponseBody,errorResponseBody} from '../utils/responsebody.js';
 const create = async(req,res) =>{
     try{
         const response = await createTheatreService(req.body);
+        if(response.err){
+            errorResponseBody.err = response.err;
+            errorResponseBody.message = "Validation failed on few parameters of the request body";
+            return res.status(response.code).json(errorResponseBody);
+        }
         successResponseBody.data = response;
         successResponseBody.message = "Successfully created the theatre";
         return res.status(201).json(successResponseBody);
--- services/theatre.service.js
+++ services/theatre.service.js
@@ -5,6 +5,13 @@ const createTheatreService = async(data)=>{
         return response;
     }
     catch(err){
+        if(error.name == 'ValidationError'){
+            let err ={};
+            Object.keys(error.errors).forEach((key)=>{
+                err[key] = error.errors[key].message;
+            });
+            return {err: err, code: 422};
+        }
         console.log(err);
         throw err;
     }
