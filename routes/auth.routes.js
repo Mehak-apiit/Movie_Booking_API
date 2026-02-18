0d4b38016199997ce47594b97dce533b702f0a40
Mehak <mehak.apiit@gmail.com>
2026-02-18 18:36:08 +0530

    Feat: to add validators for signin

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,11 +1,17 @@
 import {signup,signin} from '../controllers/auth.controller.js';
-import validateSignupRequest from '../middlewares/auth.middlewares.js';
+import {validateSigninRequest,validateSignupRequest } from '../middlewares/auth.middlewares.js';
 const authRoutes = (app) => {
     app.post(
         '/mba/api/vi/auth/signup',validateSignupRequest,signup
     );
     app.post(
         '/mba/api/vi/auth/signin',signin
+    );
+    app.post(
+        '/mba/api/vi/auth/signin',
+        validateSigninRequest,
+        signin
+
     )
 }
 export default authRoutes;
